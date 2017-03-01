0535c69d934f7f4cb4bd92a387841fa14e6e7520
Joseph Heenan <jheenan@greenkeytech.com>
2017-03-01 15:19:27 -0500

    Step1; workaroud issue with vec3 just taking 2 params

--- jth82_a7/step1.cpp
+++ jth82_a7/step1.cpp
@@ -13,32 +13,93 @@
 #include <map>
 #include <algorithm>
 #include <iterator>
+#define DEBUG 1
 
 
 mat4 B = mat4(1,-3,3,-1,0,3,-6,3,0,0,3,-1,0,0,0,1);
 mat4 Btranspose = transpose(B);
 
+float p[4][2] = {
+		{0.0,0.0},
+		{.25,1.0},
+		{.7,.9},
+		{1.0,0.0}
+};
+
 vec3 controlPoints[4] = {
 		vec3(0.0,0.0,0.0),
 		vec3(.25f,1.0,0.0),
 		vec3(.7f,.9f,0.0),
-		vec3(1.0f,0.0f)
+		vec3(1.0f,0.0f,0.0)
 };
+
 
 // Take 4 control points, and u, provide output
 vec3 calcPoint(float u) {
-	// Sum of 4 values
 
-	vec3 val1 = (1 - u) * (1 - u) * (1 - u) * controlPoints[0];
-	vec3 val2 = 3 * u * (1 - u) * (1 - u) * controlPoints[1];
-	vec3 val3 = 3 * u * u * (1 - u) * * controlPoints[2];
-	vec3 val4 = u * u * u * controlPoints[3];
-	vec3 sum = val1 + val2 + val3 + val4;
+	vec3 retVal;
+	float uu = (1 - u);
 
-	return sum;
+	//printf("xs are: %f, %f, %f, %f\n",controlPoints[0].x,controlPoints[1].x,controlPoints[2].x,controlPoints[3].x);
+	//printf("ys are: %f, %f, %f, %f\n",controlPoints[0].y,controlPoints[1].y,controlPoints[2].y,controlPoints[3].y);
 
+	retVal.x = uu * uu * uu * controlPoints[0].x
+	+ 3 * u * uu * uu * controlPoints[1].x
+	+ 3 * u * u * uu * controlPoints[2].x
+	+ u * u * u * controlPoints[3].x;
+
+	retVal.y = uu * uu * uu * controlPoints[0].y
+	+ 3 * u * uu * uu * controlPoints[1].y
+	+ 3 * u * u * uu * controlPoints[2].y
+	+ u * u * u * controlPoints[3].y;
+
+	return retVal;
 }
 
+vec3 calcPoint2() {
+	float N = 4;
+	float d = 1.0/(N-1.0);
+	float u, uu;
+	for(int i = 0; i < N; i++) {
+
+		u = i*d;
+		uu = 1.0-u;
+
+		float x = p[0][0]*uu*uu*uu
+				+ 3.0*p[1][0]*uu*uu*u
+				+ 3.0*p[2][0]*uu*u*u
+				+ p[3][0]*u*u*u;
+
+		float y = p[0][1]*uu*uu*uu
+				+ 3.0*p[1][1]*uu*uu*u
+				+ 3.0*p[2][1]*uu*u*u
+				+ p[3][1]*u*u*u;
+
+		printf("%f,%f\n",x,y);
+
+	}
+}
+
+
+float points[4][4];
+
+void calc3() {
+	int N = 4;
+	float d = 1.0/(N-1.0);  // Or 3
+	float u, uu;
+	for(int i=0;i<N;i++) {
+		u = i*d;
+		uu = 1.0 - u;
+		for(int j = 0; j < 2; j++) {
+			points[i][j] = (float) p[0][j] * uu * uu * uu
+					+ 3.0 * p[1][j] * uu * uu * u
+					+ 3.0 * p[2][j] * uu *u*u
+					+ p[3][j] * u * u * u;
+			printf("%f\n",points[i][j]);
+		}
+	}
+
+}
 
 
 int
@@ -46,11 +107,28 @@ main( int argc, char **argv )
 {
 
 	// Sample at 20 points along curve in range [0..1]
+
 	for(int i = 1; i < 21; i++) {
 		float u = (float)i/20;
 		vec3 y = calcPoint(u);
-		printf("%f,%f,%f,%f\n",u,y.x,y.y,y.z);
+		printf("%f,%f\n",y.x,y.y);
 	}
+
+
+	printf("Try 2\n");
+
+	calcPoint2();
+
+	printf("Try 3\n");
+
+	calc3();
+
+	for(int i = 0; i < 4; i++) {
+		for(int j = 0; j < 2; j++) {
+			printf("%f,%f\n",points[i][j]);
+		}
+	}
+
 
 	return 0;
 }
