57c4b2184074d33d704a39f1df956dd1fc08703e
Joseph Heenan <jheenan@greenkeytech.com>
2017-03-01 17:13:39 -0500

    Easier to read

--- jth82_a7/step1.cpp
+++ jth82_a7/step1.cpp
@@ -16,17 +16,6 @@
 #define DEBUG 1
 
 
-mat4 B = mat4(1,-3,3,-1,0,3,-6,3,0,0,3,-1,0,0,0,1);
-mat4 Btranspose = transpose(B);
-
-float p[4][2] = {
-		{0.0,0.0},
-		{.25,1.0},
-		{.7,.9},
-		{1.0,0.0}
-};
-
-
 float patch[4][4][3] = {
 		{
 			{ 0.0, 0.0, 0.0 },
@@ -53,57 +42,6 @@ float patch[4][4][3] = {
 			{ 6.0, 6.0, -0.2}
 		}
 };
-
-vec3 patchVec[16] = {
-	vec3( 0.0, 0.0, 0.0 ),
-		vec3( 2.0, 0.0, 1.5),
-		vec3( 4.0, 0.0, 2.9 ),
-		vec3( 6.0, 0.0, 0.0 ),
-		vec3( 0.0, 2.0, 1.1 ),
-		vec3( 2.0, 2.0, 3.9 ),
-		vec3( 4.0, 2.0, 3.1 ),
-		vec3( 6.0, 2.0, 0.7 ),
-		vec3( 0.0, 4.0, -0.5),
-		vec3( 2.0, 4.0, 2.6 ),
-		vec3( 4.0, 4.0, 2.4 ),
-		vec3( 6.0, 4.0, 0.4 ),
-		vec3( 0.0, 6.0, 0.3 ),
-		vec3( 2.0, 6.0, -1.1),
-		vec3( 4.0, 6.0, 1.3 ),
-		vec3( 6.0, 6.0, -0.2)
-};
-
-vec3 cpVec1[4] = {
-	vec3( 0.0, 0.0, 0.0 ),
-		vec3( 2.0, 0.0, 1.5),
-		vec3( 4.0, 0.0, 2.9 ),
-		vec3( 6.0, 0.0, 0.0 ),
-
-};
-
-vec4 cpVec2[4] = {
-				vec3( 0.0, 2.0, 1.1 ),
-				vec3( 2.0, 2.0, 3.9 ),
-				vec3( 4.0, 2.0, 3.1 ),
-				vec3( 6.0, 2.0, 0.7 )
-
-};
-
-vec4 cpVec3[4] = {
-						vec3( 0.0, 4.0, -0.5),
-						vec3( 2.0, 4.0, 2.6 ),
-						vec3( 4.0, 4.0, 2.4 ),
-						vec3( 6.0, 4.0, 0.4 )
-};
-
-vec4 cpVec4[4] = {
-		vec3( 0.0, 6.0, 0.3 ),
-		vec3( 2.0, 6.0, -1.1),
-		vec3( 4.0, 6.0, 1.3 ),
-		vec3( 6.0, 6.0, -0.2)
-};
-
-
 
 
 vec4 getBernsteinFactors(float u) {
@@ -169,71 +107,15 @@ vec4 calcPatchPoints() {
 		}
 
 	}
-
-
-
-
 }
-
-
-
-vec3 calcPoint2() {
-	float N = 4;
-	float d = 1.0/(N-1.0);
-	float u, uu;
-	for(int i = 0; i < N; i++) {
-
-		u = i*d;
-		uu = 1.0-u;
-
-		float x = p[0][0]*uu*uu*uu
-				+ 3.0*p[1][0]*uu*uu*u
-				+ 3.0*p[2][0]*uu*u*u
-				+ p[3][0]*u*u*u;
-
-		float y = p[0][1]*uu*uu*uu
-				+ 3.0*p[1][1]*uu*uu*u
-				+ 3.0*p[2][1]*uu*u*u
-				+ p[3][1]*u*u*u;
-
-		printf("%f,%f\n",x,y);
-
-	}
-}
-
-
-float points[4][4];
-
-void calc3() {
-	int N = 4;
-	float d = 1.0/(N-1.0);  // Or 3
-	float u, uu;
-	for(int i=0;i<N;i++) {
-		u = i*d;
-		uu = 1.0 - u;
-		for(int j = 0; j < 2; j++) {
-			points[i][j] = (float) p[0][j] * uu * uu * uu
-					+ 3.0 * p[1][j] * uu * uu * u
-					+ 3.0 * p[2][j] * uu *u*u
-					+ p[3][j] * u * u * u;
-			printf("%f\n",points[i][j]);
-		}
-	}
-
-}
-
-
-
-void generateBernsteins() {
-
-}
-
 
 int
 main( int argc, char **argv )
 {
 
-	/*
+
+
+	/* Working code for Bezier Curve calculation
 	// Sample at 20 points along curve in range [0..1]
 	vec3 controlPoints[4] = {
 			vec3(0.0,0.0,0.0),
@@ -247,22 +129,8 @@ main( int argc, char **argv )
 		vec3 y = calcPoint(u,controlPoints);
 		printf("%f,%f\n",y.x,y.y);
 	}
-
-
-	printf("Try 2\n");
-
-	calcPoint2();
-
-	printf("Try 3\n");
-
-	calc3();
-
-	for(int i = 0; i < 4; i++) {
-		for(int j = 0; j < 2; j++) {
-			printf("%f,%f\n",points[i][j]);
-		}
-	}
 	*/
+
 	calcPatchPoints();
 
 
